b0ebe479b2ff5091f0112db3918633257401704d
Charlie <khozacharles832@gmail.com>
2025-09-08 20:33:23 +0200

    Implement Auth providers and route protection

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://xtebnnalhjhmjexgyufu.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0ZWJubmFsaGpobWpleGd5dWZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczMTU0MzEsImV4cCI6MjA3Mjg5MTQzMX0.lBltczyWSTpnBcmDdmmXWExJEapxwaYWpDOjQ15X8MQ';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
